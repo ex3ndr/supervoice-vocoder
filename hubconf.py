5f4cbff52c6743b3da1e7adcd9eb8121155a1fc6
Steve Korshakov <steve@korshakov.com>
2024-02-14 03:52:18 +0000

    ref: add eval, change pretrained link

--- hubconf.py
+++ hubconf.py
@@ -1,10 +1,11 @@
 dependencies = ['torch', 'torchaudio']
 
 def bigvsan(pretrained=True):
+    import torch
     from supervoice_vocoder.bigvsan import BigVSAN
     model = BigVSAN()
     if pretrained:
-        checkpoint = torch.hub.load_state_dict_from_url("https://shared.korshakov.com/models/bigvsan.pt")
+        checkpoint = torch.hub.load_state_dict_from_url("https://shared.korshakov.com/models/bigvsan_10m.pt")
         model.load_state_dict(checkpoint)
     return model
             
